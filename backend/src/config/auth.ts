b0251573f14c8e4749630f0be4c85a54d87ead5d
tharlys10 <tharlys10c@hotmail.com>
2021-05-01 15:52:38 -0300

    feat: :sparkles: create middleaware ensure authentication
    criado o middleware para garatir a autenticação do usuário

---
+++ backend/src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "1ba95d9a0cfb6509a978b698065f5dc5",
+  expires_in_token: "60m",
+  secret_refresh_token: "f38d103696ebfe9ddc9635f986a294fb",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30
+}
